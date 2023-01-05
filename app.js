e2757510199f64647e0dba9049cae4f47c717294
Jason Yeon <jasonyeon110@gmail.com>
2023-01-05 23:44:29 +0900

    partial changges to the app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT;
+const tasks = require('./routes/tasks');
+
+const app = express();
+
+//middleware 
+app.use(express.json());
+
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks',tasks)
+
+app.listen(port, () => console.log(`listening on ${port}`));
